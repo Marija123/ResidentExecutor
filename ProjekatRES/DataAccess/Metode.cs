b565a82741f28eb149a56ee3b644b47b4c1a8555
Maca123 <marijadjurkovic503@gmail.com>
2018-05-18 12:41:32 +0200

    Dopuna Solution-a

---
+++ ProjekatRES/DataAccess/Metode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Metode
+    {
+        private DataIO serializer = new DataIO();
+        public static BindingList<Podatak> podaci = new BindingList<Podatak>();
+        public void UcitajUBazu1(double vrednost, string vreme)
+        {
+            string fileName = "EnteredData.xml";
+            string fullPath = Path.GetFullPath(fileName);
+            podaci.Add(new Podatak(vrednost,vreme));
+            serializer.SerializeObject<BindingList<Podatak>>(podaci, fullPath);
+        }
+    }
+}
---
+++ ProjekatRES/DataAccess/Podatak.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Podatak
+    {
+        public double vrednost { get; set; }
+        public string datum { get; set; }
+
+        public Podatak() { }
+        public Podatak(double v, string d)
+        {
+            vrednost = v;
+            datum = d;
+        }
+    }
+}
