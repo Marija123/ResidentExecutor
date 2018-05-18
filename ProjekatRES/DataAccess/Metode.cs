649fd28f57fbd9ada30ddedeb58a2dd153d1f545
Barbara96 <barbarasegrt96@gmail.com>
2018-05-18 17:35:31 +0200

    Izmena pristupa xml fajlu

--- ProjekatRES/DataAccess/Metode.cs
+++ ProjekatRES/DataAccess/Metode.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,10 +15,16 @@ namespace DataAccess
         public static BindingList<Podatak> podaci = new BindingList<Podatak>();
         public void UcitajUBazu1(double vrednost, string vreme)
         {
-            string fileName = "EnteredData.xml";
-            string fullPath = Path.GetFullPath(fileName);
+           
+            var path = Path.GetDirectoryName(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            path = Path.GetDirectoryName(path);
+            path = Path.GetDirectoryName(path);
+            path = path + @"\DataAccess\EnteredData.xml";
+            podaci = serializer.DeSerializeObject<BindingList<Podatak>>(path);
+           // string fileName = "EnteredData.xml";
+           // string fullPath = Path.GetFullPath(fileName);
             podaci.Add(new Podatak(vrednost,vreme));
-            serializer.SerializeObject<BindingList<Podatak>>(podaci, fullPath);
+            serializer.SerializeObject<BindingList<Podatak>>(podaci, path);
         }
     }
 }
