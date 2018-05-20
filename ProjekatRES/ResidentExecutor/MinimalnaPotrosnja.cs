8baaf550a9f71db831ce5d36bb48107743029afb
Barbara96 <barbarasegrt96@gmail.com>
2018-05-20 17:44:28 +0200

    Odradjene funkcije 2 i 3
    i odradjeno automatsko izvrsavanje funkcija u residentu

---
+++ ProjekatRES/ResidentExecutor/MinimalnaPotrosnja.cs
@@ -0,0 +1,118 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidentExecutor
+{
+    public class MinimalnaPotrosnja : IFunkcije
+    {
+        public Metode m = new Metode();
+        public void IzvrsiFunkciju()
+        {
+
+            List<Podrucje> lista = m.IzlistajPodrucja();
+            List<Podatak> listaPodataka = m.IscitajIzBaze();
+            Dictionary<string, string> dic = m.IzlistajPoslednje(2);
+            List<double> listaVrednosti;
+            Dictionary<string, string> listaPoslednjihVremena = new Dictionary<string, string>();
+
+
+
+            List<double> listaRez = new List<double>();
+
+            if (!dic.Any())
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    
+                    listaVrednosti = new List<double>();
+                    List<string> s = new List<string>();
+                    foreach (Podatak pod in listaPodataka)
+                    {
+
+                        if (lista[i].Sifra.Equals(pod.sifraPod))
+                        {
+                            string[] pom = pod.datum.Split(' ');
+                            s.Add(pom[1]);
+
+
+                            listaVrednosti.Add(pod.vrednost);
+                        }
+                    }
+                    listaRez.Add(listaVrednosti.Min());
+                    listaPoslednjihVremena.Add(lista[i].Sifra, s.Last());
+
+                }
+
+                m.UcitajUBazu(listaRez, listaPoslednjihVremena, DateTime.Now, 2);
+            }
+            else
+            {
+
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    List<string> s = new List<string>();
+                    foreach (Podatak pod in listaPodataka)
+                    {
+
+                        if (lista[i].Sifra.Equals(pod.sifraPod))
+                        {
+                            string[] pom = pod.datum.Split(' ');
+                            s.Add(pom[1]);
+                            
+                        }
+                    }
+
+                    listaPoslednjihVremena.Add(lista[i].Sifra, s.Last());
+
+                }
+
+                List<string> listaDrz = new List<string>();
+                foreach (string sif in dic.Keys)
+                {
+                    foreach (string l in listaPoslednjihVremena.Keys)
+                    {
+                        if (sif == l)
+                        {
+                            if (dic[sif] != listaPoslednjihVremena[l])
+                            {
+                                listaDrz.Add(sif);
+                            }
+                        }
+                    }
+                }
+
+                Dictionary<string, string> li = new Dictionary<string, string>();
+
+                for (int i = 0; i < listaDrz.Count; i++)
+                {
+                    
+                    List<string> s = new List<string>();
+                    listaVrednosti = new List<double>();
+                    foreach (Podatak pod in listaPodataka)
+                    {
+
+                        if (listaDrz[i].Equals(pod.sifraPod))
+                        {
+                            listaVrednosti.Add(pod.vrednost);
+                            string[] pom = pod.datum.Split(' ');
+                            s.Add(pom[1]);
+
+                        }
+
+                    }
+                    listaRez.Add(listaVrednosti.Min());
+                    li.Add(listaDrz[i], s.Last());
+                    // listaPoslednjihVremena.Add(lista[i].Sifra, s.Last());
+
+                }
+                m.UcitajUBazu(listaRez, li, DateTime.Now, 2);
+            }
+
+
+        }
+    }
+}
--- ProjekatRES/ResidentExecutor/Program.cs
+++ ProjekatRES/ResidentExecutor/Program.cs
@@ -1,21 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ResidentExecutor
 {
     class Program
     {
+        public static List<int> l; 
         static void Main(string[] args)
         {
             IFunkcije funkcija;
 
-            funkcija = new ProsecnaPotrosnja();
+            do
+            {
+                IscitajXML();
 
-            funkcija.IzvrsiFunkciju();
-            Console.ReadLine();
+                foreach (int i in l)
+                {
+                   if(i == 1)
+                    {
+                        funkcija = new ProsecnaPotrosnja();
+                        
+                    }
+                   else if(i == 2)
+                    {
+                        funkcija = new MinimalnaPotrosnja();
+                    }
+                    else
+                    {
+                        funkcija = new MaksimalnaPotrosnja();
+                    }
+
+                    funkcija.IzvrsiFunkciju();
+                
+                }
+                Console.WriteLine("Izvrsene funkcije.");
+                Thread.Sleep(10000);
+            } while (true);
+
         }
+
+        public static void IscitajXML()
+        {
+            l = new List<int>();
+            var path = Path.GetDirectoryName(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            path = Path.GetDirectoryName(path);
+            path = Path.GetDirectoryName(path);
+            string path1 = path + @"\FileSystem\rezidentne_funkcije.xml";
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path1);
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                string text = node.InnerText; //or loop through its children as well
+                if (text.Substring(1).Equals("1"))
+                {
+                    l.Add(Int32.Parse(text.Substring(0, 1)));
+                }
+               
+            }
+        }
+
     }
 }
